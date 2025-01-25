05c9e73957eb6c4e03b04182ac6eae3f5851b44d
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-01-24 18:35:32 -0700

    KingMoves is passing tests. Had to add the removed value for 0 base back in.

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,4 +1,98 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class KingMovesCalculator implements PieceMovesCalculator {
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+        int myRow = myPosition.getRow() + 1;
+        int myCol = myPosition.getColumn() + 1;
+
+        ChessPiece king = board.getPiece(myPosition);
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        if(myRow < 8) {
+            ChessPosition newPosition = new ChessPosition(myRow + 1, myCol);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            System.out.println(board.getPiece(newPosition));
+            System.out.println(board.getPiece(myPosition));
+
+            if( piece == null || piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+
+        }
+
+        // Decrease row by one
+        if(myRow > 1) {
+            ChessPosition newPosition = new ChessPosition(myRow - 1, myCol);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+
+        }
+
+        if(myCol < 8) {
+            ChessPosition newPosition = new ChessPosition(myRow, myCol + 1);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+
+        if(myCol > 1) {
+            ChessPosition newPosition = new ChessPosition(myRow, myCol - 1);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+
+        if(myRow < 8 && myCol < 8) {
+            ChessPosition newPosition = new ChessPosition(myRow + 1, myCol + 1);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+
+        if(myRow < 8 && myCol > 1) {
+            ChessPosition newPosition = new ChessPosition(myRow + 1, myCol - 1);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+
+        if(myRow > 1 && myCol > 1) {
+            ChessPosition newPosition = new ChessPosition(myRow - 1, myCol - 1);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+
+        if(myRow > 1 && myCol < 8) {
+            ChessPosition newPosition = new ChessPosition(myRow - 1, myCol + 1);
+            ChessPiece piece = board.getPiece(newPosition);
+
+            if( piece == null|| piece.getTeamColor() != king.getTeamColor() ) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+
+        return moves;
+
+    }
 }
