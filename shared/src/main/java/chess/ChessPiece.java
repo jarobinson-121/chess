4e5531a71f56d4088035e289e754491a5d3ef41c
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-01-27 12:48:26 -0700

    Made a small adjustment to how I switch from 0 to 1 based counting for board.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +11,34 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 
     /**
@@ -29,14 +57,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +75,28 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+//        Is a switch case the right thing to use here? If so, does that have issues with stringify? And what does it call from calcs?
+        switch (type) {
+            case KING:
+                KingMovesCalculator KingMoves = new KingMovesCalculator();
+                return KingMoves.pieceMoves(board, myPosition);
+            case QUEEN:
+                QueenMovesCalculator QueenMoves = new QueenMovesCalculator();
+                return QueenMoves.pieceMoves(board, myPosition);
+            case BISHOP:
+                BishopMovesCalculator BishopMoves = new BishopMovesCalculator();
+                return BishopMoves.pieceMoves(board, myPosition);
+            case KNIGHT:
+                KnightMovesCalculator KnightMoves = new KnightMovesCalculator();
+                return KnightMoves.pieceMoves(board, myPosition);
+            case ROOK:
+                RookMovesCalculator RookMoves = new RookMovesCalculator();
+                return RookMoves.pieceMoves(board, myPosition);
+            case PAWN:
+                PawnMovesCalculator PawnMoves = new PawnMovesCalculator();
+                return PawnMoves.pieceMoves(board, myPosition);
+//          I should probably replace the default with some error messaging
+            default: return null;
+        }
     }
 }
