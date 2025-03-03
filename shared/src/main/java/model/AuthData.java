fe2d68a58d2417ac7cd16ccbd477d8b57ea5f0a5
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-02 17:11:15 -0700

    Created AuthData Model and added getter and setter for AuthToken and Username respectively, as well as toString.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,4 +1,18 @@
 package model;
 
+import com.google.gson.*;
+
 public record AuthData(String authToken, String username) {
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 }
