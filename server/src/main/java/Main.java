ac59359ec61efe740a4451423e9befbd9b1b521c
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-10 21:32:19 -0600

    Added Memory versions of User and Auth DAOs, Added RegisterService instantiation and initialized server with those. Currently, successfully registering.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,22 @@
+import Service.RegisterService;
 import chess.*;
+import dataaccess.*;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
 
-        Server server = new Server();
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+//        TODO: Uncomment this line when you've added the memoryGameDAO and have soemthing to use it on.
+//        GameDAO gameDAO = new MemoryGameDAO();
+
+        var registerService = new RegisterService(authDAO, userDAO);
+
+        Server server = new Server(userDAO, authDAO, registerService);
 
         server.run(8080);
+
 
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
