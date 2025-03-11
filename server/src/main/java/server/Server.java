d53e4ce3773a171a2bef48b1989f2bbcafc53204
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-10 21:35:05 -0600

    Added constructor for Server and passed auth and user daos, with registerservice object. Added AddUser function and endpoint. Currently adding a user successfully.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,21 +1,47 @@
 package server;
 
+import com.google.gson.Gson;
+import Service.RegisterService;
+
+import dataaccess.DataAccessException;
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
 import spark.*;
+import model.UserData;
 
 public class Server {
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+    private final RegisterService registerService;
+
+    public Server(UserDAO userDAO, AuthDAO authDAO, RegisterService registerService) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+        this.registerService = registerService;
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+//        Spark.delete("/db", this::clearDatabase);
+        Spark.post("/user", this::addUser);
 
-        //This line initializes the server and can be removed once you have a functioning endpoint
-        Spark.init();
-
+//        Spark.awaitInitialization();
+//        //This line initializes the server and can be removed once you have a functioning endpoint
+//        Spark.init();
         Spark.awaitInitialization();
         return Spark.port();
+    }
+//    private Object clearDatabase(Request req, Response res) {
+//
+//    }
+
+    private Object addUser(Request req, Response res) throws DataAccessException {
+        var user = new Gson().fromJson(req.body(), UserData.class);
+        var auth = registerService.createUser(user.username(), user.password(), user.email());
+        return new Gson().toJson(auth);
     }
 
     public void stop() {
