aa1748703f7541545d72c31d4eece1adb292ca3e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-11 10:41:01 -0600

    Added and initialized loginService, added login endpoint and function call.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,9 +2,10 @@ package server;
 
 import com.google.gson.Gson;
 import Service.RegisterService;
-
+import Service.LoginService;
 import dataaccess.DataAccessException;
 import dataaccess.AuthDAO;
+import dataaccess.LoginRequest;
 import dataaccess.UserDAO;
 import spark.*;
 import model.UserData;
@@ -13,11 +14,13 @@ public class Server {
     private final AuthDAO authDAO;
     private final UserDAO userDAO;
     private final RegisterService registerService;
+    private final LoginService loginService;
 
-    public Server(UserDAO userDAO, AuthDAO authDAO, RegisterService registerService) {
+    public Server(UserDAO userDAO, AuthDAO authDAO, RegisterService registerService, LoginService loginService) {
         this.authDAO = authDAO;
         this.userDAO = userDAO;
         this.registerService = registerService;
+        this.loginService = loginService;
     }
 
     public int run(int desiredPort) {
@@ -27,6 +30,7 @@ public class Server {
 
 //        Spark.delete("/db", this::clearDatabase);
         Spark.post("/user", this::addUser);
+        Spark.post("/session", this::login);
 
 //        Spark.awaitInitialization();
 //        //This line initializes the server and can be removed once you have a functioning endpoint
@@ -41,6 +45,12 @@ public class Server {
     private Object addUser(Request req, Response res) throws DataAccessException {
         var user = new Gson().fromJson(req.body(), UserData.class);
         var auth = registerService.createUser(user.username(), user.password(), user.email());
+        return new Gson().toJson(auth);
+    }
+
+    private Object login(Request req, Response res) throws DataAccessException {
+        var userInfo = new Gson().fromJson(req.body(), LoginRequest.class);
+        var auth = loginService.loginUser(userInfo.username(), userInfo.password());
         return new Gson().toJson(auth);
     }
 
