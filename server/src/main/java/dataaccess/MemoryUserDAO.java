1048aeca4b873bc2ba55a2128af759840f5b689b
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-11 10:39:53 -0600

    Added and initialized loginService, added login endpoint and function call.

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -8,10 +8,6 @@ public class MemoryUserDAO implements UserDAO {
 
     final private HashMap<String, UserData> users = new HashMap<>();
 
-//    AuthData auth = new AuthData(generateToken(), username);
-//        authList.put(username, auth);
-//        return auth;
-
     public UserData addUser(UserData user) throws DataAccessException {
         users.put(user.username(), user);
         return user;
