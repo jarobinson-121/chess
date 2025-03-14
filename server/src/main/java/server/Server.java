3e980ac665e6c1497519f211700539689e773fbf
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-14 12:05:35 -0600

    Added logoutService and logout function and path.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,9 +3,11 @@ package server;
 import com.google.gson.Gson;
 import Service.RegisterService;
 import Service.LoginService;
+import Service.LogoutService;
 import dataaccess.DataAccessException;
 import dataaccess.AuthDAO;
 import dataaccess.LoginRequest;
+import dataaccess.LogoutRequest;
 import dataaccess.UserDAO;
 import spark.*;
 import model.UserData;
@@ -15,12 +17,14 @@ public class Server {
     private final UserDAO userDAO;
     private final RegisterService registerService;
     private final LoginService loginService;
+    private final LogoutService logoutService;
 
-    public Server(UserDAO userDAO, AuthDAO authDAO, RegisterService registerService, LoginService loginService) {
+    public Server(UserDAO userDAO, AuthDAO authDAO, RegisterService registerService, LoginService loginService, LogoutService logoutService) {
         this.authDAO = authDAO;
         this.userDAO = userDAO;
         this.registerService = registerService;
         this.loginService = loginService;
+        this.logoutService = logoutService;
     }
 
     public int run(int desiredPort) {
@@ -31,6 +35,7 @@ public class Server {
 //        Spark.delete("/db", this::clearDatabase);
         Spark.post("/user", this::addUser);
         Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
 
 //        Spark.awaitInitialization();
 //        //This line initializes the server and can be removed once you have a functioning endpoint
@@ -52,6 +57,12 @@ public class Server {
         var userInfo = new Gson().fromJson(req.body(), LoginRequest.class);
         var auth = loginService.loginUser(userInfo.username(), userInfo.password());
         return new Gson().toJson(auth);
+    }
+
+    private Object logout(Request req, Response res) throws DataAccessException {
+        String token = req.headers("authorization");
+        logoutService.logoutUser(token);
+        return "";
     }
 
     public void stop() {
