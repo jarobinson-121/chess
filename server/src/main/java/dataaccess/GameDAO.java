4cc66c15ab8ecac1644373d17a9bd0baba8a9829
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-03-24 11:23:04 -0600

    Added classes to GameDAO interface.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,14 @@
 package dataaccess;
 
+import model.GameData;
+
 public interface GameDAO {
+
+    GameData createGame(String gameName);
+
+    GameData getGame(String gameID);
+
+    GameData joinGame(String playerColor, String gameID);
+
+    void deleteGame(String gameID);
 }
