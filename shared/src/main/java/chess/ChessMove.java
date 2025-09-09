dbd08229120c52c917a164a7677fffdc78040a6b
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-09-09 10:40:11 -0600

    Reformatted toString function.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -61,10 +61,6 @@ public class ChessMove {
 
     @Override
     public String toString() {
-        return "ChessMove{" +
-                "startPosition=" + startPosition +
-                ", endPosition=" + endPosition +
-                ", promotionPiece=" + promotionPiece +
-                '}';
+        return String.format("[%s:%s]", startPosition, endPosition);
     }
 }
