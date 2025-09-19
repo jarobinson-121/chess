54df1d56e3e48f3b452f42a538a34dcd87e9d819
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-09-19 14:45:36 -0600

    Added calls to calculateMoves and extended Base abstract. Currently Passing.

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,4 +1,15 @@
 package chess;
 
-public class RookMovesCalculator {
+import java.util.Collection;
+import java.util.HashSet;
+
+public class RookMovesCalculator extends BaseMovesAbstract {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new HashSet<>();
+        calculateMoves(board, myPosition, 0, -1, moves, true);
+        calculateMoves(board, myPosition, -1, 0, moves, true);
+        calculateMoves(board, myPosition, 0, 1, moves, true);
+        calculateMoves(board, myPosition, 1, 0, moves, true);
+        return moves;
+    }
 }
