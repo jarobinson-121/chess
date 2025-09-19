f6ea05ca8d54d987ed9f57631b9f2447ac010f8e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-09-19 14:46:51 -0600

    Added calls to calculateMoves and extended Base abstract. Currently Passing.

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,4 +1,20 @@
 package chess;
 
-public class KingMovesCalculator {
+import java.util.Collection;
+import java.util.HashSet;
+
+public class KingMovesCalculator extends BaseMovesAbstract {
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new HashSet<>();
+        calculateMoves(board, myPosition, 0, -1, moves, false);
+        calculateMoves(board, myPosition, -1, 0, moves, false);
+        calculateMoves(board, myPosition, 0, 1, moves, false);
+        calculateMoves(board, myPosition, 1, 0, moves, false);
+        calculateMoves(board, myPosition, -1, -1, moves, false);
+        calculateMoves(board, myPosition, 1, -1, moves, false);
+        calculateMoves(board, myPosition, -1, 1, moves, false);
+        calculateMoves(board, myPosition, 1, 1, moves, false);
+        return moves;
+    }
 }
