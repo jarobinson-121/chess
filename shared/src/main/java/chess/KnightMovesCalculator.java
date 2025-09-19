53d174a758e75b3dc249be0d441918fad6fc84fa
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-09-19 14:51:08 -0600

    Added calls to and extended base calculator. Currently passing tests.

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,4 +1,27 @@
 package chess;
 
-public class KnightMovesCalculator {
+import java.util.Collection;
+import java.util.HashSet;
+
+public class KnightMovesCalculator extends BaseMovesAbstract {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new HashSet<>();
+        //left 2, down 1
+        calculateMoves(board, myPosition, -2, -1, moves, false);
+        // left 1, down 2
+        calculateMoves(board, myPosition, -1, -2, moves, false);
+        //right 1, down 2
+        calculateMoves(board, myPosition, 1, -2, moves, false);
+        //right 2, down 1
+        calculateMoves(board, myPosition, 2, -1, moves, false);
+        //right 2, up 1
+        calculateMoves(board, myPosition, 2, 1, moves, false);
+        //right 1, up 2
+        calculateMoves(board, myPosition, 1, 2, moves, false);
+        //left 2, up 1
+        calculateMoves(board, myPosition, -2, 1, moves, false);
+        //left 1, up 2
+        calculateMoves(board, myPosition, -1, 2, moves, false);
+        return moves;
+    }
 }
