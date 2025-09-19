a040c36401881b55f746545264da2f01fe3d3d70
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-09-19 14:45:12 -0600

    Added calls to calculateMoves and extended Base abstract.

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,4 +1,15 @@
 package chess;
 
-public class BishopMovesCalculator {
+import java.util.Collection;
+import java.util.HashSet;
+
+public class BishopMovesCalculator extends BaseMovesAbstract {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new HashSet<>();
+        calculateMoves(board, myPosition, -1, -1, moves, true);
+        calculateMoves(board, myPosition, 1, -1, moves, true);
+        calculateMoves(board, myPosition, -1, 1, moves, true);
+        calculateMoves(board, myPosition, 1, 1, moves, true);
+        return moves;
+    }
 }
