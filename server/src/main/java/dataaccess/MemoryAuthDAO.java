841417796c8bca9f017b314011b7c6a2fa300ebd
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-30 23:14:06 -0600

    Added a memory implementation of the authDAO and implemented stubs from userDAO.

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class MemoryAuthDAO implements AuthDAO {
+
+    private final HashMap<String, AuthData> authList = new HashMap<>();
+
+    public AuthData createAuth(String username) {
+        AuthData auth = new AuthData(generateToken(), username);
+        authList.put(auth.authToken(), auth);
+        return auth;
+    }
+
+    public AuthData getAuth(String token) {
+        return authList.get(token);
+    }
+
+    public void deleteAuth(String token) {
+        authList.remove(token);
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
 }
