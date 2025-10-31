5091eb5cf498748cbebf40dfc9c7fac32b08e3b2
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-30 23:21:11 -0600

    Added constructor and create user functions. Currently returing correct code.

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,4 +1,25 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+
 public class RegisterService {
+    private AuthDAO AuthDAO;
+    private UserDAO UserDAO;
+
+    public RegisterService(AuthDAO authDAO, UserDAO userDAO) {
+        this.AuthDAO = authDAO;
+        this.UserDAO = userDAO;
+    }
+
+    public AuthData createUser(String username, String password, String email) throws DataAccessException {
+        if (UserDAO.getUserByUsername(username) != null) {
+            throw new DataAccessException("Username already exists");
+        }
+        UserDAO.addUser(new UserData(username, password, email));
+        return AuthDAO.createAuth(username);
+    }
 }
