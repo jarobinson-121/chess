28cc3602d3f20f8840fe8a849517a5d8af8e511e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-30 23:15:55 -0600

    Added a memory implementation of the userDAO and implemented stubs from userDAO.

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,28 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+
+    final private HashMap<String, UserData> users = new HashMap<>();
+
+    public UserData addUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+        return user;
+    }
+
+    public UserData getUserByEmail(String email) throws DataAccessException {
+        return users.get(email);
+
+    }
+
+    public UserData getUserByUsername(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    public void deleteUser(String username) throws DataAccessException {
+        users.remove(username);
+    }
 }
