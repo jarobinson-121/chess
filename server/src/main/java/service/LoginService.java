32c9190194e60311d7092a90394b9734cd9afdc4
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-31 12:09:09 -0600

    Added constructor and loginUser functions.

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,27 @@
 package service;
 
+import model.AuthData;
+import dataaccess.DataAccessException;
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+
 public class LoginService {
+    private AuthDAO authDAO;
+    private UserDAO userDAO;
+
+    public LoginService(AuthDAO authDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+
+    public AuthData loginUser(String username, String password) throws DataAccessException {
+        var user = userDAO.getUserByUsername(username);
+        if (user.username() == null) {
+            throw new DataAccessException("No user found");
+        }
+        if (!user.password().equals(password)) {
+            throw new DataAccessException("Invalid credentials");
+        }
+        return authDAO.createAuth(username);
+    }
 }
