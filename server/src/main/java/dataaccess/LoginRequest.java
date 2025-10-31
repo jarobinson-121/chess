ec2d78214499d602f55233cbe2ce1ca33522cca8
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-31 12:08:09 -0600

    Added toString method and made it a record class.

--- server/src/main/java/dataaccess/LoginRequest.java
+++ server/src/main/java/dataaccess/LoginRequest.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
-public class LoginRequest {
+import com.google.gson.*;
+
+public record LoginRequest(String username, String password) {
+
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 }
