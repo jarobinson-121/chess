a883df6418fa9113429898d02cab385cb85766ba
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-30 22:49:07 -0600

    Added function stubs addUser, getUserByEmail/Username, and deleteUser.

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,15 @@
 package dataaccess;
 
-public class UserDAO {
+import model.UserData;
+
+public interface UserDAO {
+
+    UserData addUser(UserData user) throws DataAccessException;
+
+    UserData getUserByEmail(String email) throws DataAccessException;
+
+    UserData getUserByUsername(String username) throws DataAccessException;
+
+    void deleteUser(String username) throws DataAccessException;
+
 }
