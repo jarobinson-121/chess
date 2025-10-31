a83a0f40c147f356d0939f676cd52b8e60afa25a
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-31 13:53:19 -0600

    Added constructor and logout function. Currently passing correct happy path.

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,20 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+
 public class LogoutService {
+    private AuthDAO authDAO;
+
+    public LogoutService(AuthDAO authDAO) {
+        this.authDAO = authDAO;
+    }
+
+    public void logoutUser(String authToken) throws DataAccessException {
+        var auth = authDAO.getAuth(authToken);
+        if (auth.username() == null) {
+            throw new DataAccessException("No matching user");
+        }
+        authDAO.deleteAuth(authToken);
+    }
 }
