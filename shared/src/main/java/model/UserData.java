0bc85a4ad58e550d8ada43ca2215f9e593d44c8c
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-10-30 23:07:02 -0600

    Added getters for username and password

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,4 +1,19 @@
 package model;
 
-public class UserData {
+import com.google.gson.*;
+
+public record UserData(String username, String password, String email) {
+
+    public String getUsername() {
+        return username;
+    }
+
+    // might need to remove this later because security duh
+    public String getPassword() {
+        return password;
+    }
+
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 }
