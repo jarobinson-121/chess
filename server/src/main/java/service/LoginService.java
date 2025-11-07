53adb191c33572ee64d77da1d60c4da298408e97
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-11-07 15:30:20 -0700

    updated login Exception structure. Now throws ResponseException too.

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exception.ResponseException;
 import model.AuthData;
 import dataaccess.DataAccessException;
 import dataaccess.AuthDAO;
@@ -14,13 +15,10 @@ public class LoginService {
         this.userDAO = userDAO;
     }
 
-    public AuthData loginUser(String username, String password) throws DataAccessException {
+    public AuthData loginUser(String username, String password) throws DataAccessException, ResponseException {
         var user = userDAO.getUserByUsername(username);
-        if (user.username() == null) {
-            throw new DataAccessException("No user found");
-        }
-        if (!user.password().equals(password)) {
-            throw new DataAccessException("Invalid credentials");
+        if (user == null || user.username() == null || !user.password().equals(password)) {
+            throw new ResponseException(ResponseException.Code.Unauthorized, "Unauthorized");
         }
         return authDAO.createAuth(username);
     }
