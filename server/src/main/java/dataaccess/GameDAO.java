075d5ba36d7ce2db335b81d860c82844dcc42819
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-11-17 17:39:30 -0700

    Added function stubs for each upcoming function.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,18 @@
 package dataaccess;
 
-public class GameDAO {
+import model.GameData;
+
+import java.util.HashMap;
+
+public interface GameDAO {
+
+    GameData createGame(String gameName);
+
+    GameData getGame(String gameID);
+
+    GameData joinGame(String playerColor, String gameID);
+
+    HashMap<Integer, GameData> listGames();
+
+    void deleteGame(String gameID);
 }
