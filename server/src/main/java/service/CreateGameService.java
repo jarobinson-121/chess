f1c71404c4ca0f5ef171e3d3f319e3462f6f000f
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-11-17 17:45:10 -0700

    Added code to create a game. Appears to be functioning properly.

--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.AuthData;
+import model.GameData;
+
 public class CreateGameService {
+    private AuthDAO AuthDAO;
+    private GameDAO GameDAO;
+
+    public CreateGameService(AuthDAO authDAO, GameDAO gameDAO) {
+        this.AuthDAO = authDAO;
+        this.GameDAO = gameDAO;
+    }
+
+    public GameData createGame(String token, String gameName) throws DataAccessException {
+        AuthData user = AuthDAO.getAuth(token);
+        if (user == null) {
+            throw new DataAccessException("Unauthorized");
+        }
+        return GameDAO.createGame(gameName);
+    }
+
 }
