db2eca0d76516da5f40fce51191bddc8cc26edd1
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-12-02 18:41:05 -0700

    Added code to actually join the game. Appears to be functioning properly at the moment.

--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -1,4 +1,37 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.AuthData;
+import model.GameData;
+
 public class JoinGameService {
+    private AuthDAO AuthDAO;
+    private GameDAO GameDAO;
+
+    public JoinGameService(AuthDAO authDAO, GameDAO gameDAO) {
+        this.AuthDAO = authDAO;
+        this.GameDAO = gameDAO;
+    }
+
+    public void joinGame(String token, String color, Integer gameID) throws DataAccessException {
+        AuthData user = AuthDAO.getAuth(token);
+        String uname = user.username();
+        if (user == null) {
+            throw new DataAccessException("Unauthorized");
+        }
+        GameData oldGame = GameDAO.getGame(gameID);
+        if (oldGame.blackUsername() != null && oldGame.whiteUsername() != null) {
+            throw new DataAccessException("Already Taken");
+        }
+        String blackUname = oldGame.blackUsername();
+        String whiteUname = oldGame.whiteUsername();
+        if ("WHITE".equals(color) && oldGame.whiteUsername() == null) {
+            whiteUname = uname;
+        } else if ("BLACK".equals(color) && oldGame.blackUsername() == null) {
+            blackUname = uname;
+        }
+        GameDAO.updateGame(new GameData(oldGame.gameID(), whiteUname, blackUname, oldGame.gameName(), oldGame.game()));
+    }
 }
