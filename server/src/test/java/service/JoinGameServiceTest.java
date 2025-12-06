7cbe20fee118c345c48b166e70fa84ed07d4b4fe
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-12-06 16:28:52 -0700

    Created success and fail tests for joining a game.

---
+++ server/src/test/java/service/JoinGameServiceTest.java
@@ -0,0 +1,61 @@
+package service;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import exception.ResponseException;
+import model.GameData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import passoff.model.TestUser;
+
+class JoinGameServiceTest {
+
+    private static TestUser existingUser;
+
+    MemoryUserDAO userDAO = new MemoryUserDAO();
+    MemoryAuthDAO authDAO = new MemoryAuthDAO();
+    MemoryGameDAO gameDAO = new MemoryGameDAO();
+    final RegisterService registerService = new RegisterService(authDAO, userDAO);
+    final CreateGameService createGameService = new CreateGameService(authDAO, gameDAO);
+    final JoinGameService joinGameService = new JoinGameService(authDAO, gameDAO);
+
+    @BeforeAll
+    public static void init() {
+        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
+
+    }
+
+    @Test
+    @DisplayName("Valid Join")
+    void joinGameSuccess() throws ResponseException, DataAccessException {
+        var regResponse = registerService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        var createResponse = createGameService.createGame(regResponse.authToken(), "Valid Name");
+
+        joinGameService.joinGame(regResponse.authToken(), "BLACK", createResponse.gameID());
+
+        GameData game = gameDAO.getGame(createResponse.gameID());
+
+        Assertions.assertNotNull(game);
+        Assertions.assertNotNull(game.blackUsername());
+        Assertions.assertEquals(existingUser.getUsername(), game.blackUsername());
+    }
+
+    @Test
+    @DisplayName("Bad Color")
+    void joinGameBadColor() throws ResponseException, DataAccessException {
+        var regResponse = registerService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        var createResponse = createGameService.createGame(regResponse.authToken(), "Valid Name");
+
+        Assertions.assertThrows(ResponseException.class, () -> {
+            joinGameService.joinGame(regResponse.authToken(), "GREEN", createResponse.gameID());
+            ;
+        });
+    }
+
+}
