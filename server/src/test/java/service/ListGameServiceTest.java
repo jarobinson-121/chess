79970ce9f56358fc587edff036ef3f240a8e3887
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-12-06 16:48:03 -0700

    Created success and fail tests for listing games.

---
+++ server/src/test/java/service/ListGameServiceTest.java
@@ -0,0 +1,71 @@
+package service;
+
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+import exception.ResponseException;
+import model.GameData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import passoff.model.TestUser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListGameServiceTest {
+
+    private static TestUser existingUser;
+
+    MemoryUserDAO userDAO = new MemoryUserDAO();
+    MemoryAuthDAO authDAO = new MemoryAuthDAO();
+    MemoryGameDAO gameDAO = new MemoryGameDAO();
+    final RegisterService registerService = new RegisterService(authDAO, userDAO);
+    final CreateGameService createGameService = new CreateGameService(authDAO, gameDAO);
+    final ListGameService listGameService = new ListGameService(authDAO, gameDAO);
+
+    @BeforeAll
+    public static void init() {
+        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
+
+    }
+
+    @Test
+    @DisplayName("Valid empty")
+    void listGameSuccessEmpty() throws ResponseException, DataAccessException {
+        var regResponse = registerService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        var result = listGameService.listGames(regResponse.authToken());
+
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(!result.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Valid list")
+    void listGameSuccess() throws ResponseException, DataAccessException {
+        var regResponse = registerService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        createGameService.createGame(regResponse.authToken(), "Valid Name");
+
+        var result = listGameService.listGames(regResponse.authToken());
+
+        Assertions.assertNotNull(result);
+        Assertions.assertTrue(!result.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Bad Token")
+    void listGamesBadToken() throws ResponseException, DataAccessException {
+        var regResponse = registerService.createUser(existingUser.getUsername(), existingUser.getPassword(), existingUser.getEmail());
+
+        createGameService.createGame(regResponse.authToken(), "Valid Name");
+
+        Assertions.assertThrows(ResponseException.class, () -> {
+            listGameService.listGames("bad-token");
+            ;
+        });
+    }
+
+}
