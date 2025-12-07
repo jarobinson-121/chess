766ab372d847f4e6ed4f94e748de99ff61a48b6a
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-12-06 19:11:56 -0700

    Started following the patterns from petshop. Added chunks for each DAO.

---
+++ server/src/main/java/dataaccess/SQLInitializer.java
@@ -0,0 +1,50 @@
+package dataaccess;
+
+import exception.ResponseException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SQLInitializer {
+
+    private static final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256),
+              PRIMARY KEY (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  auths (
+              `username` varchar(256) NOT NULL,
+              `token` varchar(256) NOT NULL,
+              PRIMARY KEY (`token`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  games (
+              `gameID` int NOT NULL,
+              `whiteUsername` varchar(256),
+              `blackUsername` varchar(256),
+              `gameName` varchar(256) NOT NULL,
+              `gameState` TEXT NOT NULL,
+              PRIMARY KEY (`gameID`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    public static void configureDatabase() throws DataAccessException, ResponseException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
