ed13130baccea52f1e27169ae90968648715a2fe
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-12-09 14:55:18 -0700

    Added from status code function back in patterned after petshop.

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -33,4 +33,14 @@ public class ResponseException extends Exception {
             case AlreadyTakenError -> 403;
         };
     }
+
+    public static Code fromHttpStatusCode(int httpStatusCode) {
+        return switch (httpStatusCode) {
+            case 500 -> Code.ServerError;
+            case 400 -> Code.BadRequest;
+            case 401 -> Code.Unauthorized;
+            case 403 -> Code.AlreadyTakenError;
+            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
+        };
+    }
 }
