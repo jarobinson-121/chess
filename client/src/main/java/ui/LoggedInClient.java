9ddd27911c10e60c4297d0c54f88186b415edbaf
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2025-12-10 16:28:48 -0700

    Added join and list methods that are currently working. list handles formatting in this file.

--- client/src/main/java/ui/LoggedInClient.java
+++ client/src/main/java/ui/LoggedInClient.java
@@ -1,11 +1,16 @@
 package ui;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 import exception.ResponseException;
+import model.GameData;
+import model.GameListResult;
+import model.GameSummary;
 import server.ServerFacade;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
+import java.util.*;
 
 public class LoggedInClient {
 
@@ -22,8 +27,8 @@ public class LoggedInClient {
             String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
                 case "create" -> createGame(token, params);
-//                case "list" -> list(params);
-//                case "join" -> joinGame(params);
+                case "list" -> listGames(token);
+                case "join" -> joinGame(token, params);
 //                case "observe" -> observeGame(params);
                 case "logout" -> logout(token);
                 case "quit" -> "quit";
@@ -39,6 +44,51 @@ public class LoggedInClient {
             return server.createGame(token, params);
         } catch (URISyntaxException | IOException | InterruptedException ex) {
             throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    public String listGames(String token) throws ResponseException {
+        try {
+            var serverResponse = server.listGames(token);
+            GameListResult gameList = new Gson().fromJson(serverResponse, GameListResult.class);
+
+            String userFriendlyList = new String();
+            int gameNumber = 1;
+
+            for (GameSummary summary : gameList.games()) {
+
+                String whiteUsername = "(open)";
+                String blackUsername = "(open)";
+
+                if (summary.whiteUsername() != null) {
+                    whiteUsername = summary.whiteUsername();
+                }
+                if (summary.blackUsername() != null) {
+                    blackUsername = summary.blackUsername();
+                }
+
+                String formattedSummary = String.format(
+                        "%d. %-10s %-15s %-25s %-25s%n",
+                        gameNumber++, summary.gameName(),
+                        "Game ID: " + summary.gameID(),
+                        "White Player: " + whiteUsername,
+                        "Black Player: " + blackUsername
+                );
+
+                userFriendlyList += formattedSummary;
+            }
+
+            return userFriendlyList;
+        } catch (URISyntaxException | IOException | InterruptedException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
+    public String joinGame(String token, String... params) throws ResponseException {
+        try {
+            return server.joinGame(token, params);
+        } catch (URISyntaxException | IOException | InterruptedException ex) {
+            throw new ResponseException(ResponseException.fromHttpStatusCode(ex.hashCode()), ex.getMessage());
         }
     }
 
