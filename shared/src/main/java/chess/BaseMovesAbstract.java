8e530221f8eb474c260aede302bc3fd0c7553f17
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-01-12 15:00:00 -0700

    Created base move abstraction for all pieces except pawns.

---
+++ shared/src/main/java/chess/BaseMovesAbstract.java
@@ -0,0 +1,34 @@
+package chess;
+
+import java.util.Collection;
+
+public abstract class BaseMovesAbstract implements PieceMovesCalculator {
+
+    void calculateMoves(ChessBoard board, ChessPosition pos, int rowInc, int colInc,
+                        Collection<ChessMove> moves, boolean allowMult) {
+        int moveRow = pos.getRow() + rowInc;
+        int moveCol = pos.getColumn() + colInc;
+
+        if (moveRow > 8 || moveCol > 8 || moveRow < 1 || moveCol < 1) {
+            return;
+        }
+        ChessPosition movePos = new ChessPosition(moveRow, moveCol);
+        if (board.getPiece(movePos) == null) {
+            moves.add(new ChessMove(pos, movePos, null));
+        } else {
+            ChessPiece mover = board.getPiece(pos);
+            ChessPiece obstacle = board.getPiece(movePos);
+            if (mover.getTeamColor() != obstacle.getTeamColor()) {
+                moves.add(new ChessMove(pos, movePos, null));
+            }
+            allowMult = false;
+        }
+        if (allowMult) {
+            calculateMoves(board, pos, addToInc(rowInc), addToInc(colInc), moves, allowMult);
+        }
+    }
+
+    private int addToInc(int incVal) {
+        return incVal + Integer.signum(incVal);
+    }
+}
