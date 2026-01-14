11f629a2587cb31329b6e4ece9c744e57941e9ee
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-01-14 14:11:27 -0700

    Removed commented code.

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -9,9 +9,6 @@ public class KingMoveCalculator extends BaseMovesAbstract {
         Collection<ChessMove> moves = new HashSet<>();
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-//                if (i == 0 && j == 0) {
-//                    continue;
-//                }
                 calculateMoves(board, pos, i, j, moves, false);
             }
         }
