28570334f2f2bbb27b54c62c442a8dbece8ccb95
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-01-14 14:30:28 -0700

    Created BishopMovesCalculator and added code to calculate using loops. Currently passing.

---
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -0,0 +1,22 @@
+package chess;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class BishopMovesCalculator extends BaseMovesAbstract {
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition pos) {
+        Collection<ChessMove> moves = new HashSet<>();
+        List<Integer> incs = List.of(-1, 1);
+        List<Integer> invertedIncs = List.of(1, -1);
+
+        for (Integer inc : incs) {
+            for (Integer invert : invertedIncs) {
+                calculateMoves(board, pos, inc, invert, moves, true);
+            }
+        }
+        return moves;
+        //I figured it out. Much cleaner
+    }
+}
