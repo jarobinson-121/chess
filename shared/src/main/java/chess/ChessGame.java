71b3526d31017f01441b665884afe242cf925f16
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-02-06 20:38:40 -0700

    implemented getter/setter for board that I had missed.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -104,7 +104,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        gameBoard = board;
     }
 
     /**
@@ -113,7 +113,7 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return gameBoard;
     }
 
     @Override
