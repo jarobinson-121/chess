391c13d25741f3e8bd938738256eb9cd5a205b8d
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-02-18 15:03:56 -0700

    Created AuthDao interface and added method stubs for both versions of future DAOs.

---
+++ server/src/main/java/dataaccess/daomodels/AuthDao.java
@@ -0,0 +1,14 @@
+package dataaccess.daomodels;
+
+import dataaccess.DataAccessException;
+import models.AuthData;
+
+
+public interface AuthDao {
+
+    AuthData createAuth(String username);
+
+    AuthData getAuth(String token) throws DataAccessException;
+
+    void deleteAuth(String token) throws DataAccessException;
+}
