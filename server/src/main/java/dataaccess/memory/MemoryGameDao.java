6de96673c41b866db04d96345691c9945ffff670
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-02 15:13:33 -0700

    Added placeholder functions to allow for testing.

--- server/src/main/java/dataaccess/memory/MemoryGameDao.java
+++ server/src/main/java/dataaccess/memory/MemoryGameDao.java
@@ -1,4 +1,24 @@
 package dataaccess.memory;
 
-public class MemoryGameDao {
+import chess.ChessGame;
+import dataaccess.daomodels.GameDao;
+import models.GameData;
+
+public class MemoryGameDao implements GameDao {
+
+    public GameData createGame(String gameName) {
+        return null;
+    }
+
+    public GameData getGame(int gameID) {
+        return null;
+    }
+
+    public void updateGame(ChessGame game) {
+
+    }
+    
+    public void clearGames() {
+
+    }
 }
