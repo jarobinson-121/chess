41e2d60e418f5d43f42ab7ffc4b53847aa9f4174
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-02 15:06:37 -0700

    Setup register endpoint, service, and daos.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,33 @@
 package server;
 
+import dataaccess.daomodels.AuthDao;
+import dataaccess.daomodels.GameDao;
+import dataaccess.daomodels.UserDao;
+import dataaccess.memory.MemoryAuthDao;
+import dataaccess.memory.MemoryGameDao;
+import dataaccess.memory.MemoryUserDao;
+import handlers.RegisterHandler;
 import io.javalin.*;
+import service.RegisterService;
 
 public class Server {
 
     private final Javalin javalin;
+    private final AuthDao authDao;
+    private final UserDao userDao;
+    private final GameDao gameDao;
+    private final RegisterService registerService;
+
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        this.userDao = new MemoryUserDao();
+        this.authDao = new MemoryAuthDao();
+        this.gameDao = new MemoryGameDao();
+        this.registerService = new RegisterService(authDao, userDao);
+
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+
+                .post("/user", new RegisterHandler(registerService));
 
         // Register your endpoints and exception handlers here.
 
