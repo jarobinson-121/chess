399e7923c1ef63ac1fe3e1eae5df073dae607621
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-02 12:17:03 -0700

    Added a bunch to HTTP status based off of code enum.

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -1,5 +1,9 @@
 package exception;
 
+import java.util.Map;
+
+import com.google.gson.Gson;
+
 public class ResponseException extends Exception {
 
     public enum Code {
@@ -14,5 +18,18 @@ public class ResponseException extends Exception {
     public ResponseException(Code code, String message) {
         super(message);
         this.code = code;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
+    }
+
+    public int toHttpStatusCode() {
+        return switch (code) {
+            case Code.ServerError -> 500;
+            case Code.BadRequest -> 400;
+            case Code.Unauthorized -> 401;
+            case Code.AlreadyTakenError -> 403;
+        };
     }
 }
