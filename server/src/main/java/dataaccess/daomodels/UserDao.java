7d305f32d002a0796dca4d616cff4ac9720355fc
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-02 15:08:50 -0700

    Added line for throwing DataAccessException.

--- server/src/main/java/dataaccess/daomodels/UserDao.java
+++ server/src/main/java/dataaccess/daomodels/UserDao.java
@@ -1,10 +1,11 @@
 package dataaccess.daomodels;
 
+import dataaccess.DataAccessException;
 import models.UserData;
 
 public interface UserDao {
 
-    UserData createUser(String username, String password);
+    UserData createUser(UserData user) throws DataAccessException;
 
     UserData getUser(String username);
 
