c0e31fdcd7387637eca09744c1ed8c57fd27bbb5
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-02 15:15:01 -0700

    Added daos that will be used and createUser function. Currently returns the correct code.

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,4 +1,28 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.daomodels.AuthDao;
+import dataaccess.daomodels.UserDao;
+import exception.ResponseException;
+import models.AuthData;
+import models.UserData;
+
 public class RegisterService {
+
+    private final AuthDao authDao;
+    private final UserDao userDao;
+
+    public RegisterService(AuthDao authDao, UserDao userDao) {
+        this.authDao = authDao;
+        this.userDao = userDao;
+    }
+
+    public AuthData createUser(UserData user) throws ResponseException {
+        try {
+            userDao.createUser(user);
+            return authDao.createAuth(user.username());
+        } catch (DataAccessException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
 }
