d470179613490a2c75ba6dbfed73f29ea4fa10f3
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-02 11:22:09 -0700

    MemoryUserDao with first draft at createUser.

--- server/src/main/java/dataaccess/memory/MemoryUserDao.java
+++ server/src/main/java/dataaccess/memory/MemoryUserDao.java
@@ -1,4 +1,20 @@
 package dataaccess.memory;
 
-public class MemoryUserDao {
+import dataaccess.DataAccessException;
+import dataaccess.daomodels.UserDao;
+import models.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDao implements UserDao {
+
+    private final HashMap<String, UserData> userList = new HashMap<>();
+
+    public UserData createUser(UserData user) throws DataAccessException {
+        if (userList.containsKey(user.username())) {
+            throw new DataAccessException("Already taken");
+        }
+        userList.put(user.username(), user);
+        return user;
+    }
 }
