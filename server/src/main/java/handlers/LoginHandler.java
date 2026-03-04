2fec9df6c5acc1dd127f80773d64e69c53922593
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-03 22:11:34 -0700

    Created a login handler and filled it with first attempt at code. Needs testing.

---
+++ server/src/main/java/handlers/LoginHandler.java
@@ -0,0 +1,29 @@
+package handlers;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import models.AuthData;
+import models.UserData;
+import requests.LoginRequest;
+import service.LoginService;
+
+public class LoginHandler implements Handler {
+
+    private final LoginService loginService;
+
+    public LoginHandler(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    public void handle(Context ctx) throws ResponseException {
+        var user = new Gson().fromJson(ctx.body(), LoginRequest.class);
+        if (user == null || user.username() == null || user.password() == null) {
+            throw new ResponseException(ResponseException.Code.BadRequest, "Bad Request");
+        }
+        AuthData auth = loginService.loginUser(user.username(), user.password());
+        ctx.result(new Gson().toJson(auth));
+    }
+
+}
