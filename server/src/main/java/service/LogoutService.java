c632b8579f035928a224a1c9ccd8c6947740212d
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-03 23:36:57 -0700

    Added code to get logout user working. Appears to be working right now.

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,32 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.daomodels.AuthDao;
+import exception.ResponseException;
+import models.AuthData;
+
 public class LogoutService {
+
+    private final AuthDao authDao;
+
+    public LogoutService(AuthDao authDao) {
+        this.authDao = authDao;
+    }
+
+    public void logoutUser(String token) throws ResponseException {
+        AuthData auth;
+        try {
+            auth = authDao.getAuth(token);
+        } catch (DataAccessException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+        if (auth == null) {
+            throw new ResponseException(ResponseException.Code.Unauthorized, "Unauthorized");
+        }
+        try {
+            authDao.deleteAuth(token);
+        } catch (DataAccessException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
 }
