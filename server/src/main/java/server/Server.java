518c740e1f9ddb1d14a5a95779f98a549ca1aef0
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-03 22:32:44 -0700

    Added exception handler class and endpoint to properly pass exceptions along.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -6,9 +6,11 @@ import dataaccess.daomodels.UserDao;
 import dataaccess.memory.MemoryAuthDao;
 import dataaccess.memory.MemoryGameDao;
 import dataaccess.memory.MemoryUserDao;
+import exception.ResponseException;
 import handlers.LoginHandler;
 import handlers.RegisterHandler;
 import io.javalin.*;
+import io.javalin.http.Context;
 import service.LoginService;
 import service.RegisterService;
 
@@ -31,10 +33,16 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
 
                 .post("/user", new RegisterHandler(registerService))
-                .post("/session", new LoginHandler(loginService));
+                .post("/session", new LoginHandler(loginService))
+                .exception(ResponseException.class, this::exceptionHandler);
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+    private void exceptionHandler(ResponseException ex, Context ctx) {
+        ctx.status(ex.toHttpStatusCode());
+        ctx.result(ex.toJson());
     }
 
     public int run(int desiredPort) {
