7aa12db9ef0ced44fdb0e8e0e4b902a5e54a1c81
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-03 22:19:32 -0700

    Added code for the first stab at loginUser function. Needs testing.

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,27 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.daomodels.AuthDao;
+import dataaccess.daomodels.UserDao;
+import exception.ResponseException;
+import models.AuthData;
+
 public class LoginService {
+
+    private final AuthDao authDao;
+    private final UserDao userDao;
+
+    public LoginService(AuthDao authDao, UserDao userDao) {
+        this.authDao = authDao;
+        this.userDao = userDao;
+    }
+
+    public AuthData loginUser(String username, String Password) throws ResponseException {
+        try {
+            userDao.getUser(username);
+        } catch (DataAccessException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+        return authDao.createAuth(username);
+    }
 }
