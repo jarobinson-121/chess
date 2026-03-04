d3e52dd2fae74279423825c61c0fbe645a02de41
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-04 16:42:26 -0700

    Added actual game list and filled out createGame func.

--- server/src/main/java/dataaccess/memory/MemoryGameDao.java
+++ server/src/main/java/dataaccess/memory/MemoryGameDao.java
@@ -4,10 +4,17 @@ import chess.ChessGame;
 import dataaccess.daomodels.GameDao;
 import models.GameData;
 
+import java.util.HashMap;
+
 public class MemoryGameDao implements GameDao {
 
+    private HashMap<String, GameData> gameList = new HashMap<>();
+    private int nextID = 1;
+
     public GameData createGame(String gameName) {
-        return null;
+        GameData game = new GameData(nextID++, null, null, gameName, new ChessGame());
+        gameList.put(gameName, game);
+        return game;
     }
 
     public GameData getGame(int gameID) {
@@ -17,7 +24,7 @@ public class MemoryGameDao implements GameDao {
     public void updateGame(ChessGame game) {
 
     }
-    
+
     public void clearGames() {
 
     }
