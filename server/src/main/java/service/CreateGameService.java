cb10a71c3870c18a33eea21c49c444723ada8f9d
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-04 20:57:00 -0700

    Wrote code for createGame. Appears to be working at the moment.

--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -1,7 +1,13 @@
 package service;
 
+import dataaccess.DataAccessException;
 import dataaccess.daomodels.AuthDao;
 import dataaccess.daomodels.GameDao;
+import exception.ResponseException;
+import models.AuthData;
+import models.GameData;
+import models.UserData;
+import requests.CreateGameRequest;
 
 public class CreateGameService {
 
@@ -13,5 +19,19 @@ public class CreateGameService {
         this.gameDao = gameDao;
     }
 
-
+    public GameData createGame(String token, String gameName) throws ResponseException {
+        AuthData auth;
+        try {
+            auth = authDao.getAuth(token);
+        } catch (DataAccessException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+        if (auth == null) {
+            throw new ResponseException(ResponseException.Code.Unauthorized, "Error: Unauthorized");
+        }
+        if (gameName == null) {
+            throw new ResponseException(ResponseException.Code.BadRequest, "Error: BadRequest");
+        }
+        return gameDao.createGame(gameName);
+    }
 }
