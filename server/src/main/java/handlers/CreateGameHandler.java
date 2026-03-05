a68f88a56baacb7d1d0a0588c40cabc1b22ab23e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-04 20:57:32 -0700

    Added code to actually create the game and get/handle the response.

--- server/src/main/java/handlers/CreateGameHandler.java
+++ server/src/main/java/handlers/CreateGameHandler.java
@@ -1,8 +1,11 @@
 package handlers;
 
+import com.google.gson.Gson;
 import exception.ResponseException;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import models.GameData;
+import requests.CreateGameRequest;
 import service.CreateGameService;
 
 public class CreateGameHandler implements Handler {
@@ -16,8 +19,11 @@ public class CreateGameHandler implements Handler {
     public void handle(Context ctx) throws ResponseException {
         String token = ctx.header("Authorization");
         if (token == null) {
-            throw new ResponseException(ResponseException.Code.BadRequest, "Bad Request");
+            throw new ResponseException(ResponseException.Code.BadRequest, "Error: Bad Request");
         }
-
+        String gameName = new Gson().fromJson(ctx.body(), CreateGameRequest.class).gameName();
+        GameData game = createGameService.createGame(token, gameName);
+        String response = String.format("{\"gameID\": %d}", game.gameID());
+        ctx.status(200).result(response);
     }
 }
