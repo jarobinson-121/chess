540472c9991694d841241a97b93471c8a7d32b0e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-04 21:19:49 -0700

    Wrote positive and negative tests. Both work properly.

--- server/src/test/java/service/RegisterServiceTest.java
+++ server/src/test/java/service/RegisterServiceTest.java
@@ -1,4 +1,35 @@
 package service;
 
-public class RegisterServiceTest {
+import dataaccess.memory.MemoryAuthDao;
+import dataaccess.memory.MemoryUserDao;
+import exception.ResponseException;
+import models.AuthData;
+import models.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterServiceTest {
+
+    static final RegisterService registerService = new RegisterService(new MemoryAuthDao(), new MemoryUserDao());
+
+
+    @Test
+    void addUserSuccess() throws ResponseException {
+        UserData testUser = new UserData("testUsername", "password", "email");
+        AuthData auth = registerService.createUser(testUser);
+
+        assertEquals(auth.username(), testUser.username());
+        assertNotNull(auth.authToken());
+    }
+
+    @Test
+    void addUserFailTaken() throws ResponseException {
+        UserData testUser = new UserData("takenUsername", "password", "email");
+        registerService.createUser(testUser);
+
+        assertThrows(ResponseException.class, () -> {
+            registerService.createUser(testUser);
+        });
+    }
 }
