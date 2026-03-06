9d3bf02aee2cd21507ce785f49c5dfa78472b891
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-05 20:06:10 -0700

    Wrote positive and negative tests. Both work properly.

--- server/src/test/java/service/LogoutServiceTest.java
+++ server/src/test/java/service/LogoutServiceTest.java
@@ -1,4 +1,47 @@
 package service;
 
-public class LogoutServiceTest {
+import dataaccess.memory.MemoryAuthDao;
+import dataaccess.memory.MemoryGameDao;
+import dataaccess.memory.MemoryUserDao;
+import exception.ResponseException;
+import models.AuthData;
+import models.UserData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogoutServiceTest {
+    private static UserData testUser;
+    private static AuthData auth;
+
+    static final MemoryAuthDao authDao = new MemoryAuthDao();
+    static final MemoryUserDao userDao = new MemoryUserDao();
+    static final RegisterService registerService = new RegisterService(authDao, userDao);
+    static final LoginService loginService = new LoginService(authDao, userDao);
+    static final LogoutService logoutService = new LogoutService(authDao);
+
+    @BeforeAll
+    public static void init() throws ResponseException {
+        testUser = new UserData("newUser", "newUserPassword", "eu@mail.com");
+        registerService.createUser(testUser);
+        auth = loginService.loginUser(testUser.username(), testUser.password());
+    }
+
+    @Test
+    void LogoutSuccess() throws ResponseException {
+        logoutService.logoutUser(auth.authToken());
+
+        assertThrows(ResponseException.class, () -> {
+            logoutService.logoutUser(auth.authToken());
+        });
+    }
+
+    @Test
+    void LogoutFailBadToken() {
+        assertThrows(ResponseException.class, () -> {
+            logoutService.logoutUser("BadPass");
+        });
+    }
 }
