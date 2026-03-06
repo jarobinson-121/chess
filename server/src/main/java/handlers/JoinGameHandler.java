0d4df1acb8e3cbe1133616d759b741a3ad859f38
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-05 22:42:28 -0700

    Created join handler. Currently passing my tests, but not tested beyond them.

---
+++ server/src/main/java/handlers/JoinGameHandler.java
@@ -0,0 +1,28 @@
+package handlers;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import requests.JoinGameRequest;
+import service.JoinGameService;
+
+public class JoinGameHandler implements Handler {
+
+    private final JoinGameService joinGameService;
+
+    public JoinGameHandler(JoinGameService joinGameService) {
+        this.joinGameService = joinGameService;
+    }
+
+    public void handle(Context ctx) throws ResponseException {
+        String token = ctx.header("authorization");
+        if (token == null) {
+            throw new ResponseException(ResponseException.Code.BadRequest, "Error: Bad Request");
+        }
+        JoinGameRequest request = new Gson().fromJson(ctx.body(), JoinGameRequest.class);
+        String playerColor = request.playerColor();
+        int gameID = request.gameID();
+        joinGameService.JoinGame(token, playerColor, gameID);
+    }
+}
