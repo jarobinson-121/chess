fcc74bbf5fd74a1bad5effd25d8e115678d73c8a
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-11 22:30:03 -0600

    Wrote positive and negative tests for adding users. Currently passing.

---
+++ server/src/test/java/dataaccess/SQLUserDaoTests.java
@@ -0,0 +1,43 @@
+package dataaccess;
+
+import dataaccess.sql.SQLUserDao;
+import exception.ResponseException;
+import models.UserData;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQLUserDaoTests {
+
+    static SQLUserDao userDao;
+    UserData user = new UserData("username", "password", "email@website.com");
+
+    @BeforeAll
+    static void setUp() throws DataAccessException, ResponseException {
+        DatabaseManager.configureDatabase();
+        userDao = new SQLUserDao();
+    }
+
+    @BeforeEach
+    void emptyUserDB() throws DataAccessException, ResponseException {
+        userDao.clearUsers();
+    }
+
+    @Test
+    void addRetrieveUserPositive() throws DataAccessException {
+        userDao.createUser(user);
+
+        UserData retrieved = userDao.getUser(user.username());
+        assertNotNull(retrieved);
+        assertEquals(retrieved.username(), user.username());
+    }
+
+    @Test
+    void addUserDuplicateUsername() throws DataAccessException {
+        userDao.createUser(user);
+
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            userDao.createUser(user);
+        });
+    }
+}
