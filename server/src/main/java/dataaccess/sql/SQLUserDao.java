c62019c9aa946fcae788db3a74603a3a17468d82
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-11 22:29:28 -0600

    Added more code to allow for the tests I wanted. Currently passing the add and get tests. Added a readUser class and used it based off of pet shop.

--- server/src/main/java/dataaccess/sql/SQLUserDao.java
+++ server/src/main/java/dataaccess/sql/SQLUserDao.java
@@ -9,6 +9,7 @@ import dataaccess.daomodels.UserDao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static java.sql.Types.NULL;
@@ -18,19 +19,41 @@ public class SQLUserDao implements UserDao {
     public UserData createUser(UserData user) throws DataAccessException {
         String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
         try {
-            var statement = "INSERT INTO users (username, passwordHash, email) VALUES (?, ?, ?)";
+            var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
             DatabaseManager.executeUpdate(statement, user.username(), hashedPassword, user.email());
             return user;
         } catch (Exception ex) {
-            throw new DataAccessException("Error: Failed to create new user");
+            throw new DataAccessException("Error: Failed to create new user" + ex.getMessage());
         }
     }
 
-    public UserData getUser(String username) {
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users where username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException("Error: Failed to create new user" + ex.getMessage());
+        }
         return null;
     }
 
-    public void clearUsers() {
+    public void clearUsers() throws DataAccessException {
+        var statement = "DELETE FROM users";
+        DatabaseManager.executeUpdate(statement);
+    }
 
+    private UserData readUser(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var password = rs.getString("password");
+        var email = rs.getString("email");
+
+        return new UserData(username, password, email);
     }
 }
