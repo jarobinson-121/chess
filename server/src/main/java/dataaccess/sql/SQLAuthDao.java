a9b610e273dc8ff11cd7e20b0cc7b5de21fb7aa3
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-11 22:33:17 -0600

    Added code to read auth, generate the tokens, and an initial stab at creating auth.

--- server/src/main/java/dataaccess/sql/SQLAuthDao.java
+++ server/src/main/java/dataaccess/sql/SQLAuthDao.java
@@ -1,12 +1,26 @@
 package dataaccess.sql;
 
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
 import dataaccess.daomodels.AuthDao;
 import models.AuthData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
 
 public class SQLAuthDao implements AuthDao {
 
-    public AuthData createAuth(String username) {
-        return null;
+    public AuthData createAuth(String username) throws DataAccessException {
+        try {
+            String token = generateToken();
+            var statement = "INSERT INTO auths (token, username) VALUES (?, ?)";
+            DatabaseManager.executeUpdate(statement, token, username);
+            return new AuthData(token, username);
+        } catch (Exception ex) {
+            throw new DataAccessException("Error: Failed to add authorization");
+        }
     }
 
     public AuthData getAuth(String token) {
@@ -19,5 +33,16 @@ public class SQLAuthDao implements AuthDao {
 
     public void clearAuths() {
 
+    }
+
+    private String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var token = rs.getString("token");
+        var username = rs.getString("username");
+
+        return new AuthData(token, username);
     }
 }
