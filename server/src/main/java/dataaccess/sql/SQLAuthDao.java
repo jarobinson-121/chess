f14384e4cce1ba01386126cd0aaf47b5b03198e0
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-11 20:20:32 -0600

    Created SQL Dao and added stubs for the functions.

---
+++ server/src/main/java/dataaccess/sql/SQLAuthDao.java
@@ -0,0 +1,23 @@
+package dataaccess.sql;
+
+import dataaccess.daomodels.AuthDao;
+import models.AuthData;
+
+public class SQLAuthDao implements AuthDao {
+
+    public AuthData createAuth(String username) {
+        return null;
+    }
+
+    public AuthData getAuth(String token) {
+        return null;
+    }
+
+    public void deleteAuth(String token) {
+
+    }
+
+    public void clearAuths() {
+
+    }
+}
