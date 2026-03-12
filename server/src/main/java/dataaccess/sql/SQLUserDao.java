8db8de4ddac55dc32a74c886ae326320a3acb09a
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-11 20:12:11 -0600

    Created UserDao and added code for createUser.

---
+++ server/src/main/java/dataaccess/sql/SQLUserDao.java
@@ -0,0 +1,30 @@
+package dataaccess.sql;
+
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import exception.ResponseException;
+import models.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+import dataaccess.daomodels.UserDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static java.sql.Types.NULL;
+
+public class SQLUserDao implements UserDao {
+
+    public UserData createUser(UserData user) throws DataAccessException {
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        try {
+            var statement = "INSERT INTO users (username, passwordHash, email) VALUES (?, ?, ?)";
+            DatabaseManager.executeUpdate(statement, user.username(), hashedPassword, user.email());
+            return user;
+        } catch (Exception ex) {
+            throw new DataAccessException("Error: Failed to create new user");
+        }
+    }
+
+
+}
