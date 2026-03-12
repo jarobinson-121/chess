84a29dec5c03f85e32cbd70f9dae9ac6bd56682c
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-11 20:25:05 -0600

    Created SQL Dao and added stubs for the functions.

---
+++ server/src/main/java/dataaccess/sql/SQLGameDao.java
@@ -0,0 +1,29 @@
+package dataaccess.sql;
+
+import dataaccess.daomodels.GameDao;
+import models.GameData;
+
+import java.util.Collection;
+
+public class SQLGameDao implements GameDao {
+
+    public GameData createGame(String gameName) {
+        return null;
+    }
+
+    public GameData getGame(int gameID) {
+        return null;
+    }
+
+    public void updateGame(GameData newGame) {
+
+    }
+
+    public Collection<GameData> listGames() {
+        return null;
+    }
+
+    public void clearGames() {
+
+    }
+}
