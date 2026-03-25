27bb664bb83a6c824ee61507d8e0f7819a9cc9f6
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-24 23:07:47 -0600

    Added a bunch of mostly stub functions for each of the commands, as well as the text to print based on authenticated state.

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -10,12 +10,14 @@ import server.State;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static server.State.SIGNED_IN;
+import static server.State.SIGNED_OUT;
 import static ui.EscapeSequences.*;
 
 public class ChessClient {
 
     private ServerFacade server;
-    private State state = State.SIGNED_OUT;
+    private State state = SIGNED_OUT;
 
     public ChessClient(String serverUrl) {
         server = new ServerFacade(serverUrl);
@@ -52,13 +54,8 @@ public class ChessClient {
             String cmd = (tokens.length > 0) ? tokens[0] : "help";
             String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (cmd) {
-                case "register" -> registerUser(params);
-                case "login" -> login(params);
-                case "list" -> listPets();
-                case "signout" -> signOut();
-                case "adopt" -> adoptPet(params);
-                case "list" -> listGames();
-                case "quit" -> "quit";
+                case SIGNED_OUT -> evalSignedOut(cmd, params);
+                case SIGNED_IN -> evalSignedIn(cmd, params);
                 default -> help();
             };
         } catch (ResponseException ex) {
@@ -66,5 +63,93 @@ public class ChessClient {
         }
     }
 
+    public UserData registerUser(String... params) throws ResponseException {
+        if (params.length == 3) {
+            try {
+                server.registerUser(params[0], params[1], params[2]);
+            } catch (Exception ex) {
+
+            }
+        }
+        throw new ResponseException(ResponseException.Code.BadRequest, "Expected <USERNAME> <PASSWORD> <EMAIL>");
+    }
+
+    public UserData login(String... params) throws ResponseException {
+        if (params.length == 2) {
+            try {
+                return null;
+            } catch (Exception ex) {
+
+            }
+        }
+        throw new ResponseException(ResponseException.Code.BadRequest, "Expected <USERNAME> <PASSWORD>");
+    }
+
+    public GameData createGame(String... params) throws ResponseException {
+        if (params.length == 1) {
+            try {
+                return null;
+            } catch (Exception ex) {
+
+            }
+        }
+        throw new ResponseException(ResponseException.Code.BadRequest, "Expected <USERNAME> <PASSWORD>");
+    }
+
+    public GameSummaryList listGames() {
+        return null;
+    }
+
+    public GameData joinGame(String... params) throws ResponseException {
+        if (params.length == 2) {
+            try {
+                return null;
+            } catch (Exception ex) {
+
+            }
+        }
+        throw new ResponseException(ResponseException.Code.BadRequest, "Expected <USERNAME> <PASSWORD>");
+    }
+
+    public GameData observeGame(String... params) throws ResponseException {
+        if (params.length == 1) {
+            try {
+                return null;
+            } catch (Exception ex) {
+
+            }
+        }
+        throw new ResponseException(ResponseException.Code.BadRequest, "Expected <USERNAME> <PASSWORD>");
+    }
+
+    public GameData logout(String... params) throws ResponseException {
+        if (params.length == 0) {
+            try {
+                return null;
+            } catch (Exception ex) {
+
+            }
+        }
+        throw new ResponseException(ResponseException.Code.BadRequest, "Expected <USERNAME> <PASSWORD>");
+    }
+
+    public String help() {
+        if (state == SIGNED_OUT) {
+            return """
+                    - register <USERNAME> <PASSWORD> <EMAIL>
+                    - login <USERNAME> <PASSWORD>
+                    - quit
+                    """;
+        }
+        return """
+                - create <NAME>
+                - list
+                - join <ID> <WHITE | BLACK>
+                - observe <ID>
+                - logout
+                - quit
+                - help
+                """;
+    }
 
 }
