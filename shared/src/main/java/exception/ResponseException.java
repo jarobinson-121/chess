342edd801d49a22361c19fa09dc731b040b10c9e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-03-26 15:54:16 -0600

    Added codes to added from code.

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -40,6 +40,8 @@ public class ResponseException extends Exception {
         return switch (httpStatusCode) {
             case 500 -> Code.ServerError;
             case 400 -> Code.BadRequest;
+            case 401 -> Code.Unauthorized;
+            case 403 -> Code.AlreadyTakenError;
             default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
         };
     }
