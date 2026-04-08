30c20095e675545f34d4bdd07c50c3f47e9a469e
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-04-08 13:34:09 -0600

    Added code chunk from petshop to start handler and better understand it.

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,4 +1,60 @@
 package server.websocket;
 
-public class WebSocketHandler {
+import exception.ResponseException;
+import io.javalin.websocket.*;
+
+import java.io.IOException;
+
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @Override
+    public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
+        ctx.enableAutomaticPings();
+    }
+
+    @Override
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            Action action = new Gson().fromJson(ctx.message(), Action.class);
+            switch (action.type()) {
+                case ENTER -> enter(action.visitorName(), ctx.session);
+                case EXIT -> exit(action.visitorName(), ctx.session);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleClose(WsCloseContext ctx) {
+        System.out.println("Websocket closed");
+    }
+
+    private void enter(String visitorName, Session session) throws IOException {
+        connections.add(session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new Notification(Notification.Type.ARRIVAL, message);
+        connections.broadcast(session, notification);
+    }
+
+    private void exit(String visitorName, Session session) throws IOException {
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new Notification(Notification.Type.DEPARTURE, message);
+        connections.broadcast(session, notification);
+        connections.remove(session);
+    }
+
+    public void makeNoise(String petName, String sound) throws ResponseException {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            var notification = new Notification(Notification.Type.NOISE, message);
+            connections.broadcast(null, notification);
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+
 }
