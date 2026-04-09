9b0b27710c4bc2a5d2e3668e6327cc53794f3bf2
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-04-09 01:25:23 -0600

    Added functions to send  messages to various groups. Send to everyone else still needs error fixes.

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,5 +1,6 @@
 package server.websocket;
 
+import models.SessionData;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
@@ -7,24 +8,33 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Session, SessionData> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
+    public void add(Session session, SessionData data) {
+        connections.put(session, data);
     }
 
     public void remove(Session session) {
         connections.remove(session);
     }
 
-    public void broadcast(Session excludeSession, ServerMessage notification) throws IOException {
+    public void sendToEveryoneElse(Session excludeSession, ServerMessage notification) throws IOException {
         String msg = notification.toString();
-        for (Session c : connections.values()) {
+        for (Session c : connections) {
             if (c.isOpen()) {
                 if (!c.equals(excludeSession)) {
                     c.getRemote().sendString(msg);
                 }
             }
         }
+    }
+
+    public void sendToEveryone() {
+
+    }
+
+    public void privateMessage(Session userSession, ServerMessage notification) throws IOException {
+        String msg = notification.toString();
+        userSession.getRemote().sendString(msg);
     }
 }
