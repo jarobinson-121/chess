fc7fc0dc465b01e6eff846e2a724c5ecdbff92f3
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-04-09 21:40:15 -0600

    Added function stubs to the interface for load and error message types.

--- client/src/main/java/websocket/NotificationHandler.java
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -1,7 +1,14 @@
 package websocket;
 
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 
 public interface NotificationHandler {
+
+    void loadNotify(LoadGameMessage loadGameMessage);
+
+    void errorNotify(ErrorMessage errorMessage);
+
     void notify(NotificationMessage notificationMessage);
 }
