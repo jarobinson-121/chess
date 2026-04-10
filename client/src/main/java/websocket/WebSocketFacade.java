b5b3bc267dd94951322c172e758472e931e505a7
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-04-09 21:34:38 -0600

    Added code to mimic the petshop facade. The switch case is still a work in progress.

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,11 +1,52 @@
 package websocket;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
 import jakarta.websocket.*;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage typeChecker = new Gson().fromJson(message, ServerMessage.class);
+
+                    switch (typeChecker.getServerMessageType()) {
+                        case LOAD_GAME -> {
+                            LoadGameMessage loadGameMessage = new Gson().fromJson(message, LoadGameMessage.class);
+                            notificationHandler.loadNotify(loadGameMessage);
+                        }
+                    }
+                    NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
-    
+
 }
