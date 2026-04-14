9db47ac702146dec8a33f673a7b883e63eec963c
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2026-04-13 18:41:20 -0600

    Added string from and to for easier printing.

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -5,13 +5,25 @@ import chess.ChessMove;
 public class MakeMoveCommand extends UserGameCommand {
 
     private final ChessMove move;
+    private final String from;
+    private final String to;
 
-    public MakeMoveCommand(String token, Integer gameID, ChessMove move) {
+    public MakeMoveCommand(String token, Integer gameID, ChessMove move, String from, String to) {
         super(CommandType.MAKE_MOVE, token, gameID);
         this.move = move;
+        this.from = from;
+        this.to = to;
     }
 
     public ChessMove getMove() {
         return move;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
     }
 }
